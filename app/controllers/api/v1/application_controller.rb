03c646d1ca1d778ff3c6fe7c684eb6c03d8eeeab
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-01-27 04:34:52 +0800

    jwt encode decode

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,10 +1,25 @@
 class Api::V1::ApplicationController < ApplicationController
   before_action :set_default_format
+  before_action :authenticate_token!
 
   private
 
     def set_default_format
       request.format = :json
+    end
+
+    def authenticate_token!
+      payload = JsonWebToken.decode(auth_token)
+      @current_user = User.find(payload["sub"])
+    rescue JWT::ExpiredSignature
+      render json: {error: ["Auth token has expired"]}, status: :unauthorized
+    rescue JWT::DecodeError
+      render json: {error: ["Invalid auth token"]}, status: :unauthorized
+    end
+
+    def auth_token
+      # headers contains http headers, a hash like object in which key Authorization has jwt token
+      @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
     end
 
 end

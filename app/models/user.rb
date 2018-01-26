133cb90ffe67ad874591e43af9aa9cd48b89bd54
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-01-27 01:51:37 +0800

    knock done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # alter knock jwt payload
+  def to_token_payload
+    {
+      sub: id,
+      admin: true,
+      email: email
+    }
+  end
 end

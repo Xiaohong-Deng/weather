899889dd47169910404aa8f9df871519d35ff1a6
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-01-28 05:52:53 +0800

    alter devise to do jwt auth

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -3,26 +3,12 @@ class Api::V1::ApplicationController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   before_action :set_default_format
-  before_action :authenticate_token!
+  before_action :authenticate_user!
 
   private
 
     def set_default_format
       request.format = :json
-    end
-
-    def authenticate_token!
-      payload = JsonWebToken.decode(auth_token)
-      @current_user = User.find(payload["sub"])
-    rescue JWT::ExpiredSignature
-      render json: {error: ["Auth token has expired"]}, status: :unauthorized
-    rescue JWT::DecodeError
-      render json: {error: ["Invalid auth token"]}, status: :unauthorized
-    end
-
-    def auth_token
-      # headers contains http headers, a hash like object in which key Authorization has jwt token
-      @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
     end
 
 end
